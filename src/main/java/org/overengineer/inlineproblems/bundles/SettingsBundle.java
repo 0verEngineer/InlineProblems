85e6037af8612884504c4435feb7aacbcbac65e3
郑威 <zhengwei@mac.local>
2023-08-16 10:46:22 +0800

    Update settings UI with i18n support，The settings UI supports Chinese.
    This commit refactors hardcoded strings in SettingsComponent.java into resource bundles. The strings are now extracted into two properties files, SettingsBundle_en.properties and SettingsBundle_zh_CN.properties. This is intended to provide support for internationalization (i18n) and improve code maintainability. Also, a new class, SettingsBundle.java is created to manage these resource bundles dynamically using the path 'messages.SettingsBundle', which has been added to the plugin.xml file. This refactor creates a reusable and extensible mechanism for localizing the settings UI in the future.

---
+++ src/main/java/org/overengineer/inlineproblems/bundles/SettingsBundle.java
@@ -0,0 +1,35 @@
+package org.overengineer.inlineproblems.bundles;
+
+import com.intellij.DynamicBundle;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+import java.util.function.Supplier;
+
+/**
+ * The SettingsBundle class is a dynamic bundle for managing internationalized messages for the AiCodeBot project.
+ * It extends the DynamicBundle class.
+ *
+ * @author zhengwei
+ */
+public final class SettingsBundle extends DynamicBundle {
+    @NonNls
+    public static final String BUNDLE = "messages.SettingsBundle";
+    private static final SettingsBundle INSTANCE = new SettingsBundle();
+
+    private SettingsBundle() {
+        super(BUNDLE);
+    }
+
+    @NotNull
+    public static @Nls String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
+        return INSTANCE.getMessage(key, params);
+    }
+
+    @NotNull
+    public static Supplier<@Nls String> messagePointer(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
+        return INSTANCE.getLazyMessage(key, params);
+    }
+}
