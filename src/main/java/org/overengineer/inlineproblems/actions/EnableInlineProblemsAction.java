f794d23128d6125d3c4f3b0bfc653465843e7be0
Kristoffer Larsen Hopland <24739853+khopland@users.noreply.github.com>
2025-04-25 20:47:45 +0200

    added keybinding to enable a specific type of inline problem

--- src/main/java/org/overengineer/inlineproblems/actions/EnableInlineProblemsAction.java
+++ src/main/java/org/overengineer/inlineproblems/actions/EnableInlineProblemsAction.java
@@ -1,15 +1,16 @@
-package org.overengineer.inlineproblems;
+package org.overengineer.inlineproblems.actions;
 
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
 import org.jetbrains.annotations.NotNull;
+import org.overengineer.inlineproblems.DocumentMarkupModelScanner;
+import org.overengineer.inlineproblems.Notifier;
 import org.overengineer.inlineproblems.bundles.SettingsBundle;
 import org.overengineer.inlineproblems.settings.SettingsState;
 
 public class EnableInlineProblemsAction extends AnAction {
-
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
