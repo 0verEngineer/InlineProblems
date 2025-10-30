dce5c75d8a2845d492b4caeb098c67017ea03235
0verEngineer <dev@hackinger.net>
2025-10-30 12:15:49 +0100

    - Fix indentation in DocumentMarkupModelScanner - Add/Move isEnableInlineProblem check to the start of the routines - Added IPAction for a problem reset and refresh method - version and changelog

--- src/main/java/org/overengineer/inlineproblems/actions/EnableInlineProblemsAction.java
+++ src/main/java/org/overengineer/inlineproblems/actions/EnableInlineProblemsAction.java
@@ -1,16 +1,13 @@
 package org.overengineer.inlineproblems.actions;
 
 import com.intellij.notification.NotificationType;
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.ApplicationManager;
 import org.jetbrains.annotations.NotNull;
-import org.overengineer.inlineproblems.DocumentMarkupModelScanner;
 import org.overengineer.inlineproblems.Notifier;
 import org.overengineer.inlineproblems.bundles.SettingsBundle;
 import org.overengineer.inlineproblems.settings.SettingsState;
 
-public class EnableInlineProblemsAction extends AnAction {
+public class EnableInlineProblemsAction extends IPAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
@@ -25,6 +22,6 @@ public class EnableInlineProblemsAction extends AnAction {
             );
         }
         settingsState.setEnableInlineProblem(!settingsState.isEnableInlineProblem());
-        ApplicationManager.getApplication().invokeAndWait(DocumentMarkupModelScanner.getInstance()::scanForProblemsManually);
+        resetOrRefreshProblems(settingsState);
     }
 }
