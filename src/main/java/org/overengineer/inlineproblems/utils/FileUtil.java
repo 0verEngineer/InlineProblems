8566db9a5f55097fa2d491f634abcce809149f3d
0verEngineer <dev@hackinger.net>
2025-10-30 11:57:46 +0100

    - Remove nullable annotation for modern builds - Fix max file lines ignore if it is set to 0

--- src/main/java/org/overengineer/inlineproblems/utils/FileUtil.java
+++ src/main/java/org/overengineer/inlineproblems/utils/FileUtil.java
@@ -27,7 +27,8 @@ public class FileUtil {
         if (lineCount >= 0) {
             var maxFileLines = settings.getMaxFileLines();
 
-            if (maxFileLines >= 0 && lineCount >= maxFileLines) {
+            // maxFileLines == 0 => line count is ignored
+            if (maxFileLines > 0 && lineCount >= maxFileLines) {
                 ignore = true;
             }
         }
